fc7080ac839de404ce4709dda8261b8311015217
horanchikk <horanchikk@kansk-tc.ru>
2023-02-17 16:12:51 +0700

    fix types

--- src/helpers/useAxios.ts
+++ src/helpers/useAxios.ts
@@ -4,13 +4,12 @@ const ip = "http://mob.kansk-tc.ru";
 
 export function useAxios() {
   function GET(method: string, params?: string, data?: object) {
-    return axios.get(`http://mob.kansk-tc.ru/${method}/${params}`, data: data);
+    return axios.get(`http://mob.kansk-tc.ru/${method}/${params}`, data);
   }
 
   function POST(method: string, params?: string, data?: object) {
-    return axios.post(`http://mob.kansk-tc.ru/${method}/${params}`, data: data);
+    return axios.post(`http://mob.kansk-tc.ru/${method}/${params}`, data);
   }
-
 
   return { GET, POST };
 }
